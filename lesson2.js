438fb1280576713cb351941fc1825b2ad964c897
Nathan Strutz <strutz@gmail.com>
2020-08-28 22:11:59 -0400

    Probably most of lesson2 here...

--- lesson2.js
+++ lesson2.js
@@ -7,5 +7,21 @@ console.log(555 - 123 - 4567);
 // Explaining booleans
 let badAtMath = 1 > 2;
 if (badAtMath) {
-  throw "Nope";
+  throw "Crash & Burn";
 }
+
+let x = 1;
+console.log(x++); // x is 2
+console.log(x--); // x is 1
+console.log(x < 1); // false
+console.log(x <= 1); // true
+console.log((x += 2)); // x is 3
+console.log(x / 2); // 1.5
+console.log(x * 2); // 6
+console.log(x % 2); // 1 (remainder of 3/2)
+console.log(x ** 3); // 27 (3 cubed)
+console.log(x === 3); // true
+console.log(x !== 4); // true
+console.log((x = "Hello")); // x is now a string
+console.log(x === 1 || x === "Hello"); // true
+console.log(x === 1 && x === "Hello"); // false
