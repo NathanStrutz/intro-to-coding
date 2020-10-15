c549c145b50a33952187cdc7902d791c715a1e73
Nathan Strutz <strutz@gmail.com>
2020-10-14 21:33:59 -0400

    Practicing times table, added a push method. gotta get myself ready for class tomorrow.

--- lesson7-homework.js
+++ lesson7-homework.js
@@ -1,20 +1,33 @@
 // Times Tables
 {
-	// #1 - loops in loops
-	let grid = [];
-	for (let across=0; across<=12; across++) {
-		grid[across] = [];
+  // #1 - loops in loops
+  let grid = [];
+  for (let across = 0; across <= 12; across++) {
+    grid[across] = [];
 
-		for (let down=0; down<=12; down++) {
-			grid[across][down] = across * down;
-		}
-	}
-	console.table(grid);
+    for (let down = 0; down <= 12; down++) {
+      grid[across][down] = across * down;
+    }
+  }
+  console.table(grid);
 }
 
 {
-	// #2 - one-liner
-	let grid = [...Array(13)].map((_,across)=>[...Array(13)].map((_,down)=>across*down))
-	console.table(grid);
+  // #2 array pushing
+  grid = [];
+  for (let across = 0; across <= 12; across++) {
+    grid.push([]);
+    for (let down = 0; down <= 12; down++) {
+      grid[across].push(across * down);
+    }
+  }
+  console.table(grid);
 }
 
+{
+  // #3 - one-liner
+  let grid = [...Array(13)].map((_, across) =>
+    [...Array(13)].map((_, down) => across * down)
+  );
+  console.table(grid);
+}
