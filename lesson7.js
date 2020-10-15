b8f4500b0c7b3b89d7266f6c4f50463e5af27695
Nathan Strutz <strutz@gmail.com>
2020-10-14 21:29:11 -0400

    Note about lesson 7

---
+++ lesson7.js
@@ -0,0 +1,9 @@
+/**
+ * Lesson 7
+ *
+ * We kept working on the previous week's Rock-Paper-Scissors game
+ * Then I tried to do the times table and we ran out of time
+ * so our times table is now on lesson 8.
+ *
+ * There's a fluff week in here anyway, so this week becomes a wash.
+ */
