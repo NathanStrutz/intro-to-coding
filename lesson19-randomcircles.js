f37409f1a4bdc8dbafbea7770bee708c866f15cd
Nathan Strutz <strutz@gmail.com>
2021-02-17 21:17:06 -0500

    On second thought, no loop is probably better, plus clickable redrawing.

--- lesson19-randomcircles.js
+++ lesson19-randomcircles.js
@@ -2,16 +2,19 @@ let canvas = { width: window.innerWidth, height: window.innerHeight };
 
 function setup() {
   createCanvas(canvas.width, canvas.height);
-  frameRate(1);
+  // frameRate(1);
+  noLoop();
 }
 
 function draw() {
   noStroke();
   for (let j = 0; j < canvas.height + 50; j += 50) {
     for (let i = 0; i < canvas.width + 50; i += 10) {
-      //   fill(random(200, 255), random(100, 230), random(50, 200));
+      // fill(random(200, 255), random(100, 230), random(50, 200));
       fill(random(200, 255));
       ellipse(i, j, random(100));
     }
   }
 }
+
+window.mouseReleased = draw;
