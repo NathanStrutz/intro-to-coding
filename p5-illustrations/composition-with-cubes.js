c25f93374f7e5e1c8ef05368ac4ff17670c941bf
Nathan Strutz <strutz@gmail.com>
2021-02-20 15:16:20 -0500

    Secondary square rotation in composition with cubes

--- p5-illustrations/composition-with-cubes.js
+++ p5-illustrations/composition-with-cubes.js
@@ -1,5 +1,5 @@
 var setup = function () {
-  createCanvas(700, 900);
+  createCanvas(700, 950);
   noLoop();
 
   // rotate in degrees, so it works for normal math & my brain
@@ -16,7 +16,7 @@ var draw = function () {
   let special = { across: floor(random(across)), down: floor(random(down)) };
 
   // offset the starting point so we can draw outside the lines
-  translate(100, 10);
+  translate(100, 60);
   strokeWeight(2);
   for (let vertical = 0; vertical < down; vertical++) {
     for (let horizontal = 0; horizontal < across; horizontal++) {
@@ -33,14 +33,20 @@ var draw = function () {
         fill(random(255), random(255), random(255));
       }
 
-      // Rotate each box more, randomly, depending on how far down it is.
+      // Rotate each box placement, more and more as we go down
       // I used a sine to get it to change more gradually.
-      rotate(random(-vertical * 0.2 + sin(vertical), vertical * 0.2 + sin(vertical)));
+      rotate(random(-vertical * 0.2 + sin(vertical * 320), vertical * 0.2 + sin(vertical)));
+
       // Another translate to move the focus to the local area, so {1,1} is the top corner of this local area.
       // This time I'm doing it so that the rotation above affects the distance the box is from its home.
       // That way, it has more travel distance from it's regular spot, the further down the page it goes.
       translate(squareSize * horizontal, squareSize * vertical);
-      square(1, 1, squareSize);
+
+      // Rotate each box spin so they aren't straight
+      // Again, I used a sine to make the change gradual, more and more as we loop down
+      rotate(random(-vertical * log(vertical), +vertical * log(vertical)));
+
+      square(-squareSize / 2, -squareSize / 2, squareSize);
       // end the push/pop to leave the translation settings behind, localized to that last box
       pop();
     }
