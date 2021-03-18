fc7394858299d30e3d17cd66e660e8863f2b6069
Nathan Strutz <strutz@gmail.com>
2021-03-18 09:31:22 -0400

    Updated cards & prep for L23

--- cards.js
+++ cards.js
@@ -14,9 +14,8 @@ class Card {
  * A pack of cards - 52 unique cards, fresh out of the packaging
  */
 class PackOfCards {
-  constructor() {
-    this.deck = this.createDeck();
-  }
+  deck = this.createDeck();
+
   createCard(suit, rank) {
     let color = ["Hearts", "Diamonds"].includes(suit) ? "red" : "black";
 
@@ -68,30 +67,32 @@ class Dealer {
 }
 
 /**
- * Players hold their hand and decide how they play their game. They also count their wins.
+ * Something that collects points and tracks a score
  */
-class Player {
-  constructor() {
-    this.numberOfWins = 0;
-  }
+class PointCollector {
+  numberOfWins = 0;
 
+  addPoint() {
+    this.numberOfWins++;
+  }
+  getPoints() {
+    return this.numberOfWins;
+  }
+}
+
+/**
+ * Players hold their hand and decide how they play their game.
+ * They also keep score by extending the PointCollector class.
+ */
+class Player extends PointCollector {
   setHand(hand) {
     this.hand = hand;
   }
-
   getHighestCard() {
     return this.hand.reduce((card, highest) => (card.rank > highest?.rank ? card : highest), this.hand[0]);
   }
-
   getScore() {
     return this.getHighestCard().rank;
-  }
-
-  youWin() {
-    this.numberOfWins++;
-  }
-  getWins() {
-    return this.numberOfWins;
   }
 }
 
@@ -99,17 +100,15 @@ class Player {
  * The mechanics of running our card game is done with the Game class
  */
 class Game {
-  constructor() {
-    this.dealer = new Dealer();
-  }
+  dealer = new Dealer();
 
   play(iterations = 1) {
     let player1 = new Player();
     let player2 = new Player();
-    let ties = new Player(); // just to keep track of ties
+    let ties = new PointCollector(); // just to keep track of ties
 
     for (let index = 0; index < iterations; index++) {
-      // New deck for each game
+      // New deck for each round
       this.dealer.newDeck();
 
       // pass out the cards
@@ -117,31 +116,37 @@ class Game {
       player2.setHand(this.dealer.getHand(5));
 
       if (player1.getScore() > player2.getScore()) {
-        player1.youWin();
+        player1.addPoint();
       } else if (player2.getScore() > player1.getScore()) {
-        player2.youWin();
+        player2.addPoint();
       } else {
-        ties.youWin();
+        ties.addPoint();
       }
     }
 
-    console.log("Player 1 wins:", player1.getWins(), "times");
-    console.log("Player 2 wins:", player2.getWins(), "times");
-    console.log("Tied:", ties.getWins(), "times");
+    console.log("Player 1 wins:", player1.getPoints(), "times");
+    console.log("Player 2 wins:", player2.getPoints(), "times");
+    console.log("Tied:", ties.getPoints(), "times");
   }
 }
 
-// Here's how to play the game from the command line - uncomment these 2 lines:
+// To play the game from the command line, uncomment these 2 lines:
 // let game = new Game();
 // game.play(100);
 
+//
+//
 // To integrate it somewhere else, you need something like this:
+//
+//
 /**
  *
  * // Get a card dealer:
  * let dealer = new Dealer();
  *
  * // Get some cards:
+ * dealer.getCard();
+ * // or...
  * dealer.getHand();
  *
  * // If you want to determine a highest card or anything, you have to give the hand to a player:
--- lesson23-cards.js
+++ lesson23-cards.js
@@ -1,28 +1,78 @@
-let hands = [];
+class DisplayHand {
+  constructor(hand) {
+    this.plainCards = hand;
+    this.displayCards = [];
+
+    for (let index = 0; index < hand.length; index++) {
+      this.displayCards.push(new DisplayCard(hand[index]));
+    }
+  }
+
+  draw() {
+    for (let index = 0; index < this.displayCards.length; index++) {
+      const card = this.displayCards[index];
+      card.draw(index * 160 + 20, 20);
+    }
+  }
+}
+
+class DisplayCard {
+  constructor(card) {
+    this.card = card;
+  }
+
+  w = 150;
+  h = 200;
+
+  getSymbol() {
+    switch (this.card.suit) {
+      case "Diamonds":
+        return "♦";
+      case "Hearts":
+        return "♥";
+      case "Clubs":
+        return "♣";
+      case "Spades":
+        return "♠";
+    }
+  }
+
+  draw(x, y) {
+    push();
+    translate(x, y);
+
+    // paper background
+    rect(0, 0, this.w, this.h, 5);
+
+    // center symbol
+    textSize(100);
+    fill(this.card.color);
+    textAlign(CENTER, CENTER);
+    text(this.getSymbol(), 15, 0, this.w, this.h);
+
+    // Rank on the top left
+    textSize(25);
+    textAlign(LEFT, TOP);
+    text(this.card.name, 5, 5, this.w, this.h);
+
+    // Rank on the bottom right
+    textSize(25);
+    textAlign(RIGHT, BOTTOM);
+    text(this.card.name, 0, 0, this.w, this.h - 2);
+
+    pop();
+  }
+}
+
+let dealer = new Dealer();
+let hand = new DisplayHand(dealer.getHand());
 
 var setup = function () {
   createCanvas(1000, 800);
-
-  let dealer = new Dealer();
-  hands.push(dealer.getHand(5));
-  hands.push(dealer.getHand(5));
-  hands.push(dealer.getHand(5));
 };
 
 var draw = function () {
   background("forestgreen");
 
-  for (let handIndex = 0; handIndex < hands.length; handIndex++) {
-    const hand = hands[handIndex];
-    for (let cardIndex = 0; cardIndex < hand.length; cardIndex++) {
-      const card = hand[cardIndex];
-      drawCard(card);
-    }
-  }
+  hand.draw();
 };
-
-function drawCard(card) {
-  translate(card.x || 20, card.y || 20);
-  rect(150, 200);
-  fill(255);
-}
