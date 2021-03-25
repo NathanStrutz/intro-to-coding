32ec6569b81516a7a0b3cdc187d08042816a5292
Nathan Strutz <strutz@gmail.com>
2021-03-24 21:55:23 -0400

    Here's what I *want* to get done in class, but with only 1 hour, I'd be happy with half this progress.

--- website/card-game-thursday-target-1.js
+++ website/card-game-thursday-target-1.js
@@ -1,12 +1,67 @@
+class VisualHand {
+  constructor() {
+    let dealer = new Dealer();
+    let hand = dealer.getHand();
+    this.hand = [];
+    for (let index = 0; index < hand.length; index++) {
+      const card = hand[index];
+      this.hand.push(new VisualCard(card));
+    }
+  }
+
+  draw() {
+    for (let index = 0; index < this.hand.length; index++) {
+      const visualCard = this.hand[index];
+      visualCard.draw();
+    }
+  }
+
+  startMoving() {
+    for (let index = this.hand.length - 1; index >= 0; index--) {
+      const card = this.hand[index];
+      if (mouseX > card.x && mouseX < card.x + 150 && mouseY > card.y && mouseY < card.y + 200) {
+        card.startMoving();
+        break;
+      }
+    }
+    let sortFn = function (a, b) {
+      if (a.isMoving) {
+        return 1;
+      } else if (b.isMoving) {
+        return -1;
+      } else {
+        return 0;
+      }
+    };
+    this.hand.sort(sortFn);
+  }
+  stopMoving() {
+    for (let index = 0; index < this.hand.length; index++) {
+      const card = this.hand[index];
+      card.stopMoving();
+    }
+  }
+  move() {
+    for (let index = 0; index < this.hand.length; index++) {
+      const card = this.hand[index];
+      card.move();
+    }
+  }
+}
+
 class VisualCard {
   constructor(card) {
     this.card = card;
-    this.x = 20;
-    this.y = 20;
   }
 
+  x = 20;
+  y = 20;
+  isMoving = false;
+  xMouseOffset = 0;
+  yMouseOffset = 0;
+
   getSymbol() {
-    switch (card.suit) {
+    switch (this.card.suit) {
       case "Hearts":
         return "♥";
       case "Diamonds":
@@ -18,33 +73,62 @@ class VisualCard {
     }
   }
 
+  startMoving() {
+    this.isMoving = true;
+    this.xMouseOffset = mouseX - this.x;
+    this.yMouseOffset = mouseY - this.y;
+  }
+  stopMoving() {
+    this.isMoving = false;
+  }
+
   move() {
-    console.log("Moooooooove", `x: ${this.x}=${window.mouseX}`);
-    this.x = window.mouseX;
-    this.y = window.mouseY;
+    if (this.isMoving) {
+      this.x = mouseX - this.xMouseOffset;
+      this.y = mouseY - this.yMouseOffset;
+    }
   }
 
   draw() {
-    translate(this.x, this.y);
-    fill("white");
-    rect(0, 0, 150, 200, 7);
+    push();
+    {
+      translate(this.x, this.y);
+      fill("white");
+      rect(0, 0, 150, 200, 7);
 
-    fill(card.color);
-    textSize(100);
-    text(this.getSymbol(), 50, 50);
+      textAlign(CENTER);
+      fill(this.card.color);
+      textSize(100);
+      text(this.getSymbol(), 75, 130);
+
+      textSize(20);
+
+      textAlign(LEFT);
+      text(this.card.name + " " + this.getSymbol(), 10, 24);
+
+      textAlign(RIGHT);
+      text(this.getSymbol() + " " + this.card.name, 140, 190);
+    }
+    pop();
   }
 }
 
-// Get a card dealer:
-let dealer = new Dealer();
-// Get some cards:
-let card = dealer.getCard();
-let visCard = new VisualCard(card);
+let hand = new VisualHand();
 
 var setup = function () {
   createCanvas(1000, 800);
 };
 var draw = function () {
   background("forestgreen");
-  visCard.draw();
+  hand.draw();
+};
+
+window.mousePressed = function () {
+  hand.startMoving.apply(hand);
+};
+window.mouseReleased = function () {
+  hand.stopMoving.apply(hand);
+};
+window.mouseDragged = function () {
+  hand.move.apply(hand);
 };
