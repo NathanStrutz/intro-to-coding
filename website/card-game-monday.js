a08f0c8f14af90062d6bfa0cc7dac6dcbde75bc3
Nathan Strutz <strutz@gmail.com>
2021-03-29 19:20:15 -0400

    Monday progress

--- website/card-game-monday.js
+++ website/card-game-monday.js
@@ -1,4 +1,8 @@
 class VisualCard {
+  /**
+   *
+   * @param {Card} card
+   */
   constructor(card) {
     this.card = card;
   }
@@ -6,25 +10,50 @@ class VisualCard {
   x = 20;
   y = 20;
 
-  draw() {
-    fill("white");
-    rect(this.x, this.y, 150, 200, 7);
+  getIcon() {
+    if (this.card.suit === "Hearts") {
+      return "♥";
+    } else if (this.card.suit === "Diamonds") {
+      return "♦";
+    } else if (this.card.suit === "Spades") {
+      return "♠";
+    } else if (this.card.suit === "Clubs") {
+      return "♣";
+    }
+  }
 
-    // ♥ ♦ ♠ ♣
+  draw() {
+    translate(this.x, this.y);
+
+    fill("white");
+    rect(0, 0, 150, 200, 7);
+
     fill(this.card.color);
     textSize(100);
-    text("♣", 100, 100);
+    textAlign(CENTER);
+    text(this.getIcon(), 75, 125);
+
+    textSize(20);
+    textAlign(LEFT);
+    text(this.card.name, 15, 30);
+
+    textSize(20);
+    textAlign(RIGHT);
+    text(this.card.name, 135, 170);
   }
 }
 
 let dealer = new Dealer();
-let card = dealer.getCard();
-let visualCard = new VisualCard(card);
+let hand = dealer.getHand();
+let visualHand = hand.map((card) => new VisualCard(card));
 
 var setup = function () {
   createCanvas(1000, 800);
 };
 var draw = function () {
   background("forestgreen");
-  visualCard.draw();
+  for (let index = 0; index < visualHand.length; index++) {
+    const visualCard = visualHand[index];
+    visualCard.draw();
+  }
 };
