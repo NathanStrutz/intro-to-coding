639ab4edf29fcbc73eee8f3648d436f06edbf3bc
Nathan Strutz <strutz@gmail.com>
2021-03-29 17:51:31 -0400

    Thursday progress

--- website/card-game-thursday.js
+++ website/card-game-thursday.js
@@ -1,22 +1,56 @@
+class VisualCard {
+  constructor(card) {
+    this.card = card;
+  }
+
+  x = 20;
+  y = 20;
+
+  getIcon() {
+    if (this.card.suit === "Diamonds") {
+      return "♦";
+    } else if (this.card.suit === "Hearts") {
+      return "♥";
+    } else if (this.card.suit === "Clubs") {
+      return "♣";
+    } else if (this.card.suit === "Spades") {
+      return "♠";
+    }
+  }
+
+  draw() {
+    translate(this.x, this.y);
+
+    fill("white");
+    rect(0, 0, 150, 200, 7);
+
+    fill(this.card.color);
+    textSize(100);
+    textAlign(CENTER);
+    text(this.getIcon(), 75, 130);
+
+    textSize(20);
+    textAlign(LEFT);
+    text(this.card.name, 10, 10);
+
+    textAlign(RIGHT);
+    text(this.card.name, 150, 180);
+  }
+}
+
 // Get a card dealer:
 let dealer = new Dealer();
-// Get some cards:
-let card = dealer.getCard();
 
-let drawCard = function (card) {
-  fill("white");
-  rect(20, 20, 150, 200, 7);
+let visCard1 = new VisualCard(dealer.getCard());
 
-  // ♥ ♦ ♠ ♣
-  fill(card.color);
-  textSize(100);
-  text("♣", 50, 50);
-};
+let visCard2 = new VisualCard(dealer.getCard());
+visCard2.x = 200;
 
 var setup = function () {
   createCanvas(1000, 800);
 };
 var draw = function () {
   background("forestgreen");
-  drawCard(card);
+  visCard1.draw();
+  visCard2.draw();
 };
