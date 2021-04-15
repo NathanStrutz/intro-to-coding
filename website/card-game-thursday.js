6a9ffee071c4f7456faf512adcb669f15f922c92
Nathan Strutz <strutz@gmail.com>
2021-04-15 18:12:16 -0400

    final version from in class

--- website/card-game-thursday.js
+++ website/card-game-thursday.js
@@ -6,11 +6,24 @@ class VisualCard {
   x = 20;
   y = 20;
   isDragging = false;
+  xOffset = 0;
+  yOffset = 0;
+
+  dragging() {
+    if (mouseX > this.x && mouseX < this.x + 150 && mouseY > this.y && mouseY < this.y + 200) {
+      this.isDragging = true;
+      this.xOffset = mouseX - this.x;
+      this.yOffset = mouseY - this.y;
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   drag() {
     if (this.isDragging) {
-      this.x = mouseX;
-      this.y = mouseY;
+      this.x = mouseX - this.xOffset;
+      this.y = mouseY - this.yOffset;
     }
   }
 
@@ -30,15 +43,41 @@ class VisualCard {
     this.drag();
     push();
     {
+      // move the x/y to this card's location
       translate(this.x, this.y);
 
+      // The card background
       fill("white");
+      strokeWeight(2);
       rect(0, 0, 150, 200, 7);
 
-      fill(this.card.color);
-      textSize(100);
+      // The suit icon in the middle
       textAlign(CENTER);
-      text(this.getIcon(), 75, 130);
+      fill(this.card.color);
+
+      // But first, something special.
+      // A rotated number of translucent icons around the main one, based on the rank
+      if (this.card.name === "Ace") {
+        // but not for aces
+        textSize(150);
+        text(this.getIcon(), 75, 140);
+      } else {
+        push();
+        {
+          fill(red(this.card.color), green(this.card.color), blue(this.card.color), 20);
+          textSize(50);
+          translate(75, 100); // center of card
+          for (let index = 0; index < this.card.rank; index++) {
+            text(this.getIcon(), 0, -40);
+            rotate(360 / this.card.rank);
+          }
+        }
+        pop();
+
+        // and the main icon
+        textSize(100);
+        text(this.getIcon(), 75, 130);
+      }
 
       // The corners
       textSize(20);
@@ -53,7 +92,7 @@ class VisualCard {
       push();
       {
         translate(150, 200);
-        rotate(PI * 3);
+        rotate(180);
         text(this.getCornerName() + "\n" + this.getIcon(), 16, 24);
       }
       pop();
@@ -62,11 +101,13 @@ class VisualCard {
   }
 }
 
-// Get a card dealer:
+// Get a card dealer
 let dealer = new Dealer();
-
+// and a hand of cards (just the data)
 let hand = dealer.getHand();
+// turn that hand of cards into a hand of visual cards
 let visualHand = hand.map((c) => new VisualCard(c));
+// space them out a bit
 visualHand.forEach((vc, i) => (vc.x = i * 170 + 20));
 
 var setup = function () {
@@ -74,12 +115,17 @@ var setup = function () {
 };
 var draw = function () {
   background("forestgreen");
+  angleMode(DEGREES);
+
+  // draw all the cards
   visualHand.forEach((vc) => vc.draw());
 };
 
 window.mousePressed = function () {
-  // visCard1.isDragging = true;
+  // mouse is down, let's try dragging everything
+  visualHand.forEach((vc) => vc.dragging());
 };
 window.mouseReleased = function () {
-  // visCard1.isDragging = false;
+  // mouse is up, stop dragging everything
+  visualHand.forEach((vc) => (vc.isDragging = false));
 };
