8339db1e2908df6eb260c34b23b24a3e3088bd28
Nathan Strutz <strutz@gmail.com>
2021-10-20 23:15:36 -0400

    Showing how javascript can solve for x

---
+++ learning.js
@@ -0,0 +1,17 @@
+console.log("Hello World!");
+
+let x = 1;
+console.log(x++);                       // x is 2
+console.log(x--);                       // x is 1
+console.log(x < 1);                     // false
+console.log(x <= 1);                    // true
+console.log(x += 2);                    // x is 3
+console.log(x / 2);                     // 1.5
+console.log(x * 2);                     // 6
+console.log(x % 2);                     // 1 (remainder of 3/2)
+console.log(x ** 3);                    // 27 (3 cubed)
+console.log(x === 3);                   // true
+console.log(x !== 3);                   // false
+console.log(x === 1 || x === 3);        // true
+console.log(x === 1 && x === 3);        // false
+
