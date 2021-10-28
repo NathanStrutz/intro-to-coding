288ae7fc10de55f71d19b9c86c97c894e2a69b68
Nathan Strutz <strutz@gmail.com>
2021-10-28 00:13:19 -0400

    Code for class

--- learning.js
+++ learning.js
@@ -1,6 +1,8 @@
 console.log("Hello World!");
 
 let x = 1;
+// prettier-ignore
+{
 console.log(x++);                       // x is 2
 console.log(x--);                       // x is 1
 console.log(x < 1);                     // false
@@ -14,4 +16,23 @@ console.log(x === 3);                   // true
 console.log(x !== 3);                   // false
 console.log(x === 1 || x === 3);        // true
 console.log(x === 1 && x === 3);        // false
+}
+
+console.log("----------------------------------------------------");
+
+let y = 1;
+y = 2;
+{
+  let y = 3;
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+for (let index = 0; index < 10; index++) {
+    
+}
+
 
