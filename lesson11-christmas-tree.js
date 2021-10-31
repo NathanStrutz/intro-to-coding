e88db09310a6baebc28a7acf589e825e269e5c84
Nathan Strutz <strutz@gmail.com>
2021-10-31 09:50:45 -0400

    A couple ways to build the christmas tree.

---
+++ lesson11-christmas-tree.js
@@ -0,0 +1,15 @@
+{
+  let tree = "";
+  for (let index = 0; index < 7; index++) {
+    tree = tree + "#";
+    console.log(tree);
+  }
+  console.log("#");
+}
+
+{
+  for (let tree = ""; tree.length < 8; tree += "#") {
+    console.log(tree);
+  }
+  console.log("#");
+}
