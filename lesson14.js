d1a5507ffac10e9ad223ac0487d73ca86b5f89af
Nathan Strutz <strutz@gmail.com>
2022-01-05 22:57:38 -0500

    Throw the rest of lesson 14 in here so we can see it in class

--- lesson14.js
+++ lesson14.js
@@ -5,3 +5,12 @@ let fun = function (times = 1) {
   }
 };
 fun(5);
+
+// Math Stuff
+console.log(Math.PI);
+console.log(Math.floor(Math.random() * 10));
+
+// Date Stuff
+let d = new Date("12/31/2021");
+d.setMinutes(200);
+console.log(d);
