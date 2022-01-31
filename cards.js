0d29643fd0a861acd3db7bf2f458d464b0a9537e
Nathan Strutz <strutz@gmail.com>
2022-01-30 22:14:17 -0500

    Starting spot for the 2nd week of cards

--- cards.js
+++ cards.js
@@ -1,48 +1,19 @@
-let createCard = function (suit, rank) {
-  let color = getSuitColor(suit);
-  let name = getRankName(rank);
-
-  let card = {
-    rank,
-    suit,
-    name,
-    color,
-  };
-  return card;
+// From last week
+let card = {
+  rank: 2,
+  suit: "Hearts",
+  name: "2",
+  color: "red",
 };
+console.table(card);
 
-let getRankName = function (rank) {
-  if (rank === 11) {
-    return "Jack";
-  } else if (rank === 12) {
-    return "Queen";
-  } else if (rank === 13) {
-    return "King";
-  } else if (rank === 14) {
-    return "Ace";
-  } else {
-    return rank.toString();
-  }
-};
+// New this week
+let buildDeck = function () {};
+let createCard = function () {};
+let getSuitColor = function () {};
+let getRankName = function () {};
+let getCard = function () {};
+let getHand = function () {};
 
-let getSuitColor = function (suit) {
-  if (suit === "Spades" || suit === "Clubs") {
-    return "black";
-  } else {
-    return "red";
-  }
-};
-
-let createDeck = function () {
-  let deck = [];
-  for (let i = 2; i < 15; i++) {
-    deck.push(createCard("Hearts", i));
-    deck.push(createCard("Spades", i));
-    deck.push(createCard("Diamonds", i));
-    deck.push(createCard("Clubs", i));
-  }
-  return deck;
-};
-
-let deck = createDeck();
+let deck = buildDeck();
 console.table(deck);
