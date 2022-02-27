2b9424391f056af2db3b972cae37ecd63ac89dc3
Nathan Strutz <strutz@gmail.com>
2022-02-26 21:05:54 -0500

    Add all the rules for high-card

--- cards-oo.js
+++ cards-oo.js
@@ -1,6 +1,4 @@
 class Deck {
-  cards;
-
   constructor() {
     this.cards = this.buildDeck();
   }
@@ -78,28 +76,106 @@ class Dealer {
 }
 
 class Player {
-  name;
-  hand;
-  wins = 0;
-
   constructor(name) {
     this.name = name;
+    this.hand = null;
+    this.wins = 0;
+  }
+
+  getHighestCard() {
+    if (!this.hand) {
+      console.log(`${this.name} doesn't have any cards!`);
+    } else {
+      return this.hand.reduce((highest, card) => (card.rank > highest.rank ? card : highest), this.hand[0]);
+    }
   }
 }
 
 class Game {
-  dealer = new Dealer();
-  players = [
-    new Player("Nathan"),
-    new Player("Alanda"),
-    new Player("Samantha"),
-  ];
-
   constructor() {
+    this.dealer = new Dealer();
+    this.players = [
+      new Player("Nathan"),
+      new Player("Alanda"),
+      new Player("Samantha"),
+      new Player("Jude"),
+      new Player("Aubri"),
+      new Player("Elly"),
+    ];
+
     this.deal();
   }
 
   deal() {
-    this.players.forEach((p) => (p.hand = dealer.getHand()));
+    this.players.forEach((p) => (p.hand = this.dealer.getHand()));
+  }
+
+  play(beLoud = true) {
+    // 1 Find the highest card
+    const highestCard = this.players
+      .map((p) => p.getHighestCard())
+      .reduce((best, curr) => (curr.rank > best.rank ? curr : best), {
+        /* rank 0 card that will be easily bested by a real card */
+        rank: 0,
+      });
+
+    // 2. Find who all matches this highest card
+    const winners = this.players.filter((player) => player.getHighestCard().rank === highestCard.rank);
+
+    // 3. Increase the win count for each person
+    winners.forEach((player) => player.wins++);
+
+    if (beLoud) {
+      let highCard = winners[0].getHighestCard();
+      let aOrAn = function (word) {
+        return ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"].includes(word[0]) ? "an" : "a";
+      };
+      //   a. If only one person matches, they are the winner
+      //   b. If more, everyone is actually a loser
+      if (winners.length === 1) {
+        console.log(
+          `Congratulations, ${winners[0].name} on your amazing victory with the ${highCard.name} of ${highCard.suit}!`
+        );
+      } else {
+        let winnerNames = winners.map((p) => p.name).join(" and ");
+        console.log(`It was a tie! ${winnerNames} had ${aOrAn(highCard.name)} ${highCard.name}.`);
+      }
+    }
+  }
+
+  playALot(times = 10) {
+    for (let playCount = 0; playCount < times; playCount++) {
+      this.dealer.getNewDeck();
+      this.deal();
+      this.play(times < 50);
+    }
+  }
+
+  announceWinner() {
+    // 1. Determine the number of wins that a winner won with
+    const topWins = this.players.reduce((mostWins, player) => (player.wins > mostWins ? player.wins : mostWins), 0);
+
+    // 2. Determine the player or players with that many wins
+    const winners = this.players.filter((p) => p.wins === topWins);
+
+    // 3. Congratulate the winner(s)
+    if (winners.length === 1) {
+      console.log(`The big winner was ${winners[0].name} with a stunnnig ${winners[0].wins} wins!`);
+    } else {
+      let winnerString = winners.map((w) => w.name).join(" and ");
+      console.log(`It was a tie between ${winnerString} who each had ${winners[0].wins} wins!`);
+    }
+  }
+
+  printWinnersTable() {
+    console.log(`-------------------- WINNERS - WINNERS - WINNERS - WINNERS ---------------------`);
+    console.log(`--------------------------------------------------------------------------------`);
+    console.table(this.players.map((p) => ({ name: p.name, wins: p.wins })));
+    console.log(`--------------------------------------------------------------------------------`);
   }
 }
+
+let game = new Game();
+game.playALot(1000);
+game.printWinnersTable();
+game.announceWinner();
