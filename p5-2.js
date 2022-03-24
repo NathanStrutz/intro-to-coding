4ce4765ee9c4fc85fdd85af6e4b45ed6ffe0e549
Nathan Strutz <strutz@gmail.com>
2022-03-23 22:36:58 -0400

    I think we are ready for class. It will be basically commenting things back in, stepping through the file.

--- p5-2.js
+++ p5-2.js
@@ -2,28 +2,44 @@
 
 var setup = function () {
   createCanvas(600, 400);
-  angleMode(DEGREES);
 };
 
 // 1: Move the circle
-// let x = 0;
-// var draw = function () {
-//   background(100);
-//   fill("red");
-//   circle(x++, 100, 100);
-// };
+let x = 0;
+var draw = function () {
+  background(100);
+  fill("red");
+  circle(x++, 100, 100);
+};
+
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+// **
 
 // 2: Track the circle's x-y coordinates
-// let pos = {
-//   // short for Position
+// let shape = {
 //   x: 0,
 //   y: 0,
 // };
 // var draw = function () {
 //   background(100);
 //   fill("red");
-//   circle(pos.x++, pos.y++, 100);
+//   circle(shape.x++, shape.y++, 100);
 // };
+
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+// **
 
 // 3: Center the shape and introduce random x/y movement
 // let shape = {
@@ -36,4 +52,176 @@ var setup = function () {
 //   circle((shape.x += random(-4, 4)), (shape.y += random(-4, 4)), 100);
 // };
 
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+
 // 4: Start tracking velocity
+// let shape = {
+//   x: 300,
+//   y: 200,
+//   vx: -1,
+//   vy: 1,
+// };
+// var draw = function () {
+//   background(100);
+//   fill("red");
+//   circle((shape.x += shape.vx), (shape.y += shape.vy), 100);
+// };
+
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+
+// 5: Bounce off the wall
+// let shape = {
+//   x: 300,
+//   y: 200,
+//   vx: -3,
+//   vy: -3,
+// };
+// var draw = function () {
+//   background(100);
+//   fill("red");
+//   circle((shape.x += shape.vx), (shape.y += shape.vy), 100);
+//   if (shape.x < 0 || shape.x > 600) {
+//     shape.vx = -shape.vx;
+//   }
+//   if (shape.y < 0 || shape.y > 400) {
+//     shape.vy = -shape.vy;
+//   }
+// };
+
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+
+// 6: Shapes that draw themselves
+// let shape = {
+//   x: 300,
+//   y: 200,
+//   vx: -7,
+//   vy: -6,
+//   draw: function () {
+//     fill("red");
+//     circle((shape.x += shape.vx), (shape.y += shape.vy), 100);
+//     if (shape.x < 0 || shape.x > 600) {
+//       shape.vx = -shape.vx;
+//     }
+//     if (shape.y < 0 || shape.y > 400) {
+//       shape.vy = -shape.vy;
+//     }
+//   },
+// };
+
+// var draw = function () {
+//   background(100);
+//   shape.draw();
+// };
+
+/*
+  What about 2 shapes?
+  They will need to reference "this"
+  What else references "this"?
+*/
+
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+
+// 7: Shapes that are classes
+// class Circle {
+//   constructor(vx = 1, vy = 1, color) {
+//     this.x = 300;
+//     this.y = 200;
+//     this.vx = vx;
+//     this.vy = vy;
+//     this.color = color;
+//   }
+//   draw() {
+//     fill(this.color);
+//     circle((this.x += this.vx), (this.y += this.vy), 100);
+//     if (this.x < 0 || this.x > 600) {
+//       this.vx = -this.vx;
+//     }
+//     if (this.y < 0 || this.y > 400) {
+//       this.vy = -this.vy;
+//     }
+//   }
+// }
+
+// let shape1 = new Circle(5, 5, "red");
+// let shape2 = new Circle(-4, -4, "blue");
+
+// var draw = function () {
+//   background(100);
+//   shape1.draw();
+//   shape2.draw();
+// };
+
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+
+// 8: One Thousand shapes
+// class Circle {
+//   constructor() {
+//     this.x = random(600);
+//     this.y = random(400);
+//     this.vx = random(-4, 4);
+//     this.vy = random(-4, 4);
+//     this.color = random(255);
+//   }
+
+//   draw() {
+//     fill(this.color);
+//     circle((this.x += this.vx), (this.y += this.vy), 40);
+//     if (this.x < 0 || this.x > 600) {
+//       this.vx = -this.vx;
+//     }
+//     if (this.y < 0 || this.y > 400) {
+//       this.vy = -this.vy;
+//     }
+//   }
+// }
+
+// let shapes = [];
+// var setup = function () {
+//   createCanvas(600, 400);
+//   for (let i = 0; i < 1000; i++) {
+//     shapes.push(new Circle());
+//   }
+// };
+
+// var draw = function () {
+//   background(100);
+//   for (let index = 0; index < shapes.length; index++) {
+//     const shape = shapes[index];
+//     shape.draw();
+//   }
+// };
