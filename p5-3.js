10cafacb21100c5869c53ec37101c0e568576f39
Nathan Strutz <strutz@gmail.com>
2022-03-30 23:20:30 -0400

    Ready for class

--- p5-3.js
+++ p5-3.js
@@ -5,27 +5,27 @@ var setup = function () {
 };
 
 // 6: Shapes that draw themselves
-// let shape = {
-//   x: 300,
-//   y: 200,
-//   vx: -7,
-//   vy: -6,
-//   draw: function () {
-//     fill("red");
-//     circle((shape.x += shape.vx), (shape.y += shape.vy), 100);
-//     if (shape.x < 0 || shape.x > 600) {
-//       shape.vx = -shape.vx;
-//     }
-//     if (shape.y < 0 || shape.y > 400) {
-//       shape.vy = -shape.vy;
-//     }
-//   },
-// };
+let shape = {
+  x: 300,
+  y: 200,
+  vx: -7,
+  vy: -6,
+  draw: function () {
+    fill("red");
+    circle((shape.x += shape.vx), (shape.y += shape.vy), 100);
+    if (shape.x < 0 || shape.x > 600) {
+      shape.vx = -shape.vx;
+    }
+    if (shape.y < 0 || shape.y > 400) {
+      shape.vy = -shape.vy;
+    }
+  },
+};
 
-// var draw = function () {
-//   background(100);
-//   shape.draw();
-// };
+var draw = function () {
+  background(100);
+  shape.draw();
+};
 
 /*
   What about 2 shapes?
@@ -118,3 +118,148 @@ var setup = function () {
 //     shape.draw();
 //   }
 // };
+
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+
+// 9. How about Different types of shapes?
+// class Circle {
+//   constructor() {
+//     this.x = random(600);
+//     this.y = random(400);
+//     this.vx = random(-4, 4);
+//     this.vy = random(-4, 4);
+//     this.color = random(255);
+//   }
+
+//   draw() {
+//     fill(this.color);
+//     circle((this.x += this.vx), (this.y += this.vy), 40);
+//     if (this.x < 0 || this.x > 600) {
+//       this.vx = -this.vx;
+//     }
+//     if (this.y < 0 || this.y > 400) {
+//       this.vy = -this.vy;
+//     }
+//   }
+// }
+
+// class Square {
+//   constructor() {
+//     this.x = random(600);
+//     this.y = random(400);
+//     this.vx = random(-4, 4);
+//     this.vy = random(-4, 4);
+//     this.color = random(255);
+//   }
+
+//   draw() {
+//     fill(this.color);
+//     square((this.x += this.vx), (this.y += this.vy), 40);
+//     if (this.x < 0 || this.x > 600) {
+//       this.vx = -this.vx;
+//     }
+//     if (this.y < 0 || this.y > 400) {
+//       this.vy = -this.vy;
+//     }
+//   }
+// }
+
+// let shapes = [];
+// var setup = function () {
+//   createCanvas(600, 400);
+//   for (let i = 0; i < 200; i++) {
+//     shapes.push(new Circle());
+//     shapes.push(new Square());
+//   }
+// };
+
+// var draw = function () {
+//   background(100);
+//   for (let index = 0; index < shapes.length; index++) {
+//     const shape = shapes[index];
+//     shape.draw();
+//   }
+// };
+
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+
+// 10. Reduce and reuse code with inheritance
+// class Shape {
+//   constructor() {
+//     this.x = random(600);
+//     this.y = random(400);
+//     this.vx = random(-4, 4);
+//     this.vy = random(-4, 4);
+//     this.color = random(255);
+//   }
+
+//   draw() {
+//     fill(this.color);
+//     if (this.x < 0 || this.x > 600) {
+//       this.vx = -this.vx;
+//     }
+//     if (this.y < 0 || this.y > 400) {
+//       this.vy = -this.vy;
+//     }
+//   }
+// }
+// class Circle extends Shape {
+//   draw() {
+//     super.draw();
+//     circle((this.x += this.vx), (this.y += this.vy), 40);
+//   }
+// }
+
+// class Square extends Shape {
+//   draw() {
+//     super.draw();
+//     square((this.x += this.vx), (this.y += this.vy), 40);
+//   }
+// }
+
+// let shapes = [];
+// var setup = function () {
+//   createCanvas(600, 400);
+//   for (let i = 0; i < 200; i++) {
+//     shapes.push(new Circle());
+//     shapes.push(new Square());
+//   }
+// };
+
+// var draw = function () {
+//   background(100);
+//   for (let index = 0; index < shapes.length; index++) {
+//     const shape = shapes[index];
+//     shape.draw();
+//   }
+// };
+
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+// **
+
+/*
+Some ideas for where to go next if we have extra time:
+- Draw the squares in different colors
+- Make the circles move slower
+- Fix the box boundaries so the edges bounce off the walls instead of the top-left corner
+*/
