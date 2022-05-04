58ff16fc2b3a0f09d8db3b5caca3a4b2bcc5e451
Nathan Strutz <strutz@gmail.com>
2022-05-04 09:52:49 -0400

    Organize student work and add azeriah

---
+++ pong-student/azeriah/p5-5.js
@@ -0,0 +1,15 @@
+///<reference path="../../lib/p5.global-mode.d.ts" />
+
+let game;
+var setup = function () {
+  createCanvas(windowWidth, windowHeight - 40);
+  game = new Pong();
+};
+var draw = function () {
+  game.draw();
+};
+
+var windowResized = function () {
+  resizeCanvas(windowHeight, windowWidth - 4);
+  game = new Pong();
+};
