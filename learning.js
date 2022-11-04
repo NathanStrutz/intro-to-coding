6cf4029adaab7f3060cf03b72b39815ccc982b7f
Nathan Strutz <strutz@gmail.com>
2022-11-03 23:15:29 -0400

    Thinking of some ways to do this week's homework

--- learning.js
+++ learning.js
@@ -5,3 +5,24 @@ console.log("Hello World!");
 console.log("Nathan " + "Strutz");
 console.log("123-555-1234");
 console.log(123 - 555 - 1234);
+
+// 10
+let cl = console.log;
+{
+  let branches = "#";
+  for (let index = 0; index < 7; index++) {
+    cl(branches);
+    branches += "#";
+  }
+  cl("#");
+}
+{
+  let branches = "#";
+  let leftSpace = " ".repeat(6);
+  for (let index = 0; index < 5; index++) {
+    cl(leftSpace + branches);
+    branches += "##";
+    leftSpace = " ".repeat(5 - index);
+  }
+  cl("     ##");
+}
