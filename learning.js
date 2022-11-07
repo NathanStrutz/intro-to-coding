a5cee9bdb389ff54dab6e4aaf7582b86b0f1b7eb
Nathan Strutz <strutz@gmail.com>
2022-11-07 17:34:32 -0500

    Functions to play with

--- learning.js
+++ learning.js
@@ -9,6 +9,7 @@ console.log(123 - 555 - 1234);
 // 10 Trees
 let cl = console.log;
 {
+  // half tree
   let branches = "#";
   for (let index = 0; index < 7; index++) {
     cl(branches);
@@ -17,6 +18,7 @@ let cl = console.log;
   cl("#");
 }
 {
+  // full tree
   let branches = "#";
   let leftSpace = " ".repeat(6);
   for (let index = 0; index < 5; index++) {
@@ -26,3 +28,37 @@ let cl = console.log;
   }
   cl("     ##");
 }
+cl("");
+
+// 11 Loops and functions
+let christmasTree = function (height) {
+  let branches = "#";
+  for (let index = 0; index < height; index++) {
+    console.log(branches);
+    branches += "#";
+  }
+  console.log("#");
+};
+christmasTree(7);
+christmasTree(75);
+
+// Math functions
+let add = function (a, b) {
+  return a + b;
+};
+let subtract = function (a, b) {
+  return a - b;
+};
+let multiply = function (a, b) {
+  return a * b;
+};
+let divide = function (a, b) {
+  return a / b;
+};
+// tests
+cl("add", add(1, 2) == 3);
+cl("add", add(10, 15) == 25);
+cl("subtract", subtract(10, 15) == -5);
+cl("subtract", subtract(15, 10) == 5);
+cl("multiply", multiply(5, 10) == 50);
+cl("divide", divide(15, 5) == 3);
