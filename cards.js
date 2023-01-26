da705cad969e3e70653246d717bdac8f4e78c6ec
Nathan Strutz <strutz@gmail.com>
2023-01-26 00:34:46 -0500

    Start the cards project

---
+++ cards.js
@@ -0,0 +1,16 @@
+// Creates an array of all 52 cards
+let buildDeck = function () {};
+// Builds a single card based on its rank and suit
+let createCard = function (rank, suit) {};
+// Gets the card’s name based on its rank
+let getName = function (rank) {};
+// Gets the card’s color based on its suit
+let getColor = function (suit) {};
+
+let deck = buildDeck();
+console.table(deck);
+
+// Provides an array of 5 cards from the deck
+let dealHand = function (deck) {};
+// Retrieves single card from the deck
+let dealCard = function (deck) {};
