148e4343ae3c9a2bce57db1e8758161a00da8830
Nathan Strutz <strutz@gmail.com>
2023-02-12 15:24:12 -0500

    3 cards burned when there's a war-tie

--- cards-war.js
+++ cards-war.js
@@ -88,24 +88,25 @@ let gameRound = function (player1, player2) {
   if (!card2) return player1;
   if (card1.rank > card2.rank) {
     cl(player1.name, "wins a", card2.name, "of", card2.suit, "with a", card1.name);
-    return winRound(player1, card1, card2);
+    return winRound(player1, [card1, card2]);
   } else if (card1.rank < card2.rank) {
     cl(player2.name, "wins a", card1.name, "of", card1.suit, "with a", card2.name);
-    return winRound(player2, card1, card2);
+    return winRound(player2, [card1, card2]);
   } else {
     // It's a tie! Go to WAR!
     cl(`Tie breaker over ${card2.name}s`);
     let warWinner = gameRound(player1, player2);
     cl(warWinner.name, "wins the tie breaker");
-    return winRound(warWinner, card1, card2);
+    let p1BurnCards = [getTopCard(player1.hand), getTopCard(player1.hand), getTopCard(player1.hand)];
+    let p2BurnCards = [getTopCard(player2.hand), getTopCard(player2.hand), getTopCard(player2.hand)];
+    return winRound(warWinner, [card1, card2, ...p1BurnCards, ...p2BurnCards]);
   }
 };
 let getTopCard = function (hand) {
   return hand.splice(0, 1)[0];
 };
-let winRound = function (player, card1, card2) {
-  player.hand.unshift(card1);
-  player.hand.unshift(card2);
+let winRound = function (player, cards) {
+  player.hand.unshift(...cards.filter((card) => card));
   player.wins++;
   return player;
 };
@@ -118,15 +119,9 @@ gameLoop(player1, player2, rounds);
 cl("Game Over!");
 if (player2.hand.length === 0) {
   cl(player1.name, "wins with", player1.wins, "individual victories");
-  // ct(player1.hand);
 } else if (player1.hand.length === 0) {
   cl(player2.name, "wins with", player2.wins, "individual victories");
-  // ct(player2.hand);
 } else {
   cl(`The game was called off after a long and boring ${rounds} rounds`);
   cl("Where did we go so wrong?");
-  // ct(player1);
-  // ct(player1.hand);
-  // ct(player2);
-  // ct(player2.hand);
 }
